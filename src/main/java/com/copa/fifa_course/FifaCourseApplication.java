d8389c00a5ba81570770c38ed85a1f0f2373b2b9
João Ricardo Pagano Pessanha <joao.ricardopagano@gmail.com>
2022-11-30 18:07:08 -0300

    Initial commit

---
+++ src/main/java/com/copa/fifa_course/FifaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.copa.fifa_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/copa/fifa_course/FifaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copa.fifa_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
